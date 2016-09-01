03f142047724f05ca1eee8cd0a96da743797b776
Alexandre Thomaz <aleslash@gmail.com>
2016-09-01 00:20:40 -0300

    adicionando bodyparser e configurando padrao json

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var peludos = require('./controllers/peludos');
 var momentosPeludos = require('./controllers/momentosPeludos');
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
 
 app = peludos(app);
 app = momentosPeludos(app);
